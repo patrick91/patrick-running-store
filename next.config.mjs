8696cb8f99956dfdb61b761518e9859e925f07c2
Patrick Arminio <patrick.arminio@gmail.com>
2024-08-09 23:33:52 +0200

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/cdn.fourthwall.com/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
